f8e6f84a6cc7d481260ffd621e40db32e13a3c4a
lineh <linehcm@hotmail.com>
2017-08-11 09:38:52 -0400

    Envio dos primeiros arquivos
    Versão 1.0 do projeto 01.

---
+++ src/main/java/br/com/etqpadrao/etqpadrao/EtqpadraoApplication.java
@@ -0,0 +1,12 @@
+package br.com.etqpadrao.etqpadrao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtqpadraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtqpadraoApplication.class, args);
+	}
+}
